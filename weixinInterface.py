845b77d59176f53c0115503947a7adba7f8cefb9
FlynnScif <hydehyde111@163.com>
2016-08-04 00:52:30 +0800

    1

--- weixinInterface.py
+++ weixinInterface.py
@@ -35,11 +35,17 @@ class WeixinInterface:
             return echostr
 
 
-    def POST(self):        
-        str_xml = web.data() #获得post来的数据
-        xml = etree.fromstring(str_xml)#进行XML解析
-        content=xml.find("Content").text#获得用户所输入的内容
-        msgType=xml.find("MsgType").text
-        fromUser=xml.find("FromUserName").text
-        toUser=xml.find("ToUserName").text
-        return self.render.reply_text(fromUser,toUser,int(time.time()),u"我现在还在开发中，还没有什么功能，您刚才说的是："+content)
+    def POST(self): 
+        str_xml = web.data() #获得post来的数据 
+        xml = etree.fromstring(str_xml)#进行XML解析 
+        msgType=xml.find("MsgType").text 
+        fromUser=xml.find("FromUserName").text 
+        toUser=xml.find("ToUserName").text 
+        if msgType == 'text':
+            content=xml.find("Content").text
+            if(content == u"天气"):
+                pass
+        elif msgType == 'image':
+            pass
+        else:
+            pass
