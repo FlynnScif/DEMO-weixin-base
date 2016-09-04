8361f422193c386678b4964d9a73326bc83d4f50
FlynnScif <hydehyde111@163.com>
2016-09-04 13:38:20 +0800

    1

--- weixinInterface.py
+++ weixinInterface.py
@@ -35,3 +35,16 @@ class WeixinInterface:
             return echostr
 
 
+    def POST(self): 
+    str_xml = web.data() #获得post来的数据 
+    xml = etree.fromstring(str_xml)#进行XML解析 
+    msgType=xml.find("MsgType").text 
+    fromUser=xml.find("FromUserName").text 
+    toUser=xml.find("ToUserName").text 
+    if msgType == 'text':
+        content=xml.find("Content").text
+        return self.render.reply_text(fromUser,toUser,int(time.time()), content)
+    elif msgType == 'image':
+        pass
+    else:
+        pass
