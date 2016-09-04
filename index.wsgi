0eca85fe8d76b0547dc4ac7df4938f7cb6d2c6fe
FlynnScif <hydehyde111@163.com>
2016-09-04 12:56:28 +0800

    1

--- index.wsgi
+++ index.wsgi
@@ -10,8 +10,8 @@ urls = (
 )
 
 app_root = os.path.dirname(__file__)
-templates_root = os.path.join(app_root, 'templates')
+templates_root = os.path.join(app_root,'templates')
 render = web.template.render(templates_root)
 
-app = web.application(urls, globals()).wsgifunc()        
+app = web.application(urls,globals()).wsgifunc()        
 application = sae.create_wsgi_app(app)
--- weixinInterface.py
+++ weixinInterface.py
@@ -10,7 +10,7 @@ class WeixinInterface:
 
     def __init__(self):
         self.app_root = os.path.dirname(__file__)
-        self.templates_root = os.path.join(self.app_root, 'templates')
+        self.templates_root = os.path.join(self.app_root,'templates')
         self.render = web.template.render(self.templates_root)
 
     def GET(self):
@@ -37,7 +37,7 @@ class WeixinInterface:
 
     def POST(self): 
         str_xml = web.data() #获得post来的数据 
-        xml = etree.fromstring(str_xml)#进行XML解析 
+        xml = etree.fromstring(str_xml) #进行XML解析 
         msgType=xml.find("MsgType").text 
         fromUser=xml.find("FromUserName").text 
         toUser=xml.find("ToUserName").text 
