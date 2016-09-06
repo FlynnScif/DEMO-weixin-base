a54bcbc0e368e779f37c959e01ea3c99238d3026
FlynnScif <hydehyde111@163.com>
2016-09-06 23:11:30 +0800

    1

--- weixinInterface.py
+++ weixinInterface.py
@@ -37,13 +37,13 @@ class WeixinInterface:
 
 
     def POST(self): 
-        str_xml = web.data() #获得post来的数据 
-        xml = etree.fromstring(str_xml)#进行XML解析 
-        content = xml.find("Content").text 
-        msgType = xml.find("MsgType").text 
-        fromUser = xml.find("FromUserName").text 
-        toUser = xml.find("ToUserName").text 
-        retun self.render.reply_text(fromUser,toUser,int(time.time()),content)
+      #  str_xml = web.data() #获得post来的数据 
+      #  xml = etree.fromstring(str_xml)#进行XML解析 
+      #  content = xml.find("Content").text 
+      #  msgType = xml.find("MsgType").text 
+     #   fromUser = xml.find("FromUserName").text 
+      #  toUser = xml.find("ToUserName").text 
+     #   retun self.render.reply_text(fromUser,toUser,int(time.time()),content)
         #if msgType == 'text':
         #    content = xml.find("Content").text
         #    return self.render.reply_text(fromUser,toUser,int(time.time()), content)
@@ -51,4 +51,5 @@ class WeixinInterface:
         #    pass
         #else:
         #    pass
+        return '11111'
 
